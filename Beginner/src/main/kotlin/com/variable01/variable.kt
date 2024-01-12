0662beed78ca8d1d449e002ca808e5efafccbb8c
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2024-01-12 20:47:52 +0900

    Create variable.kt

---
+++ Beginner/src/main/kotlin/com/variable01/variable.kt
@@ -0,0 +1,15 @@
+package com.variable01
+
+fun main() {
+
+    //상수같이 재할당 불가능
+    val number=10
+
+    //재할당 가능
+    var item=30
+    item=45
+
+    println("number : $number")
+    println("item : $item")
+
+}
